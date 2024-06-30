9684343b630e1cee399bbc0dece4379764b01c93
Yash-Medhane <yashmedhane43@gmail.com>
2024-06-30 16:11:42 +0530

    first commit

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const body_parser = require("body-parser");
+const bodyParser = require("body-parser");
 
-app.use(body_parser.text());
+// Middleware
+app.use(bodyParser.text());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/Project", { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/Project";
 
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
     console.log("Connected to MongoDB");
 });
-
-
-// Middleware
-app.use(express.static(path.join(__dirname, "/public")));
 
 // Routes
 const userRoute = require("./routes/userRoute");
@@ -26,16 +26,15 @@ app.use("/", userRoute);
 const adminRoute = require("./routes/adminRoute");
 app.use("/adminlogin", adminRoute);
 
-// Mount question routes
 const questionRoutes = require("./routes/questionRoutes");
-
-app.use('/quiz',questionRoutes);
+app.use('/quiz', questionRoutes);
 
 const lessonRoute = require("./routes/lessonroute");
 app.use("/", lessonRoute);
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running at ${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
+
+module.exports = app;
